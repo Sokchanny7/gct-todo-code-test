23e38218f1c561996c80eebe639480594f8f0692
Sokchanny7 <sokchanny.sg@gmail.com>
2021-07-25 10:14:23 +0700

    erro add list

---
+++ src/config/app_config.js
@@ -0,0 +1,5 @@
+const AppConfig = {
+  MY_TODO_LIST_KEY: 'MY_TODO_LIST'
+};
+
+export default AppConfig;
--- src/pages/todo_page.js
+++ src/pages/todo_page.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import '../style.css';
+import React, { useEffect, useState } from 'react';
+import AppConfig from '../config/app_config';
+import _ from 'lodash';
 
-const TodoPage = () => {
+const TodoPage = props => {
+  const [todoList, setTodoList] = useState([]);
+  const [todoText, setTodoText] = useState('Hel');
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
+
+  const fetchTodoList = () => {
+    const list = localStorage.getItem(AppConfig.MY_TODO_LIST_KEY);
+    console.log('>list', list);
+    // if (list != undefined && list.length > 0) setTodoList(list);
+  };
+
+  const handleAdd = () => {
+    localStorage.setItem(AppConfig.MY_TODO_LIST_KEY, [...todoList, todoText]);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <div />
-      <input />
+      <table>
+        <thead>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th align="left">Todo</th>
+        </thead>
+        <tbody>
+          {todoList &&
+            todoList.map((item, index) => {
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td align="left">Tov jis kong</td>
+              </tr>;
+            })}
+        </tbody>
+      </table>
+      <div>
+        <div>
+          <input type="text" onChage={setTodoText} value={todoText} />
+        </div>
+        <div>
+          <button onClick={handleAdd}>Add</button>
+        </div>
+      </div>
     </div>
   );
 };
